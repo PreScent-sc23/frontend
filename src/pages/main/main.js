30de627f2e9c857a241787e8b412ab41220e1571
choiyunz <yyy0304@ajou.ac.kr>
2023-11-12 21:55:44 +0900

    [DESIGN]회원가입 UI 디자인 일부 수정

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -11,10 +11,12 @@ function StartScreen(){
     return (
         <div>
             <div className={styles.LogoContainer}>메인 로고 이미지(추후 삽입 예정)</div>
+            <div/>
+            <div/>
             <div className={styles.ButtonContainer}>
                 <div className={styles.Button} onClick={()=>navigate('/login')}>로그인</div>
-                <div className={styles.Button} /*onClick={()=>navigate('/customersigninup')}*/>일반 회원 회원가입</div>
-                <div className={styles.Button} /*onClick={()=>navigate('/sellersignup')}*/>사업자 회원 회원가입</div>
+                <div className={styles.Button} onClick={()=>navigate('/customersignup')}>일반 회원 회원가입</div>
+                <div className={styles.Button} onClick={()=>navigate('/sellersignup')}>사업자 회원 회원가입</div>
             </div>
         </div>
     )
