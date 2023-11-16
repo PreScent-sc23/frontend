c488ddae530bdcf778c006a6b8b71aea24d12786
lord-potato <148340414+lord-potato@users.noreply.github.com>
2023-11-16 18:45:46 +0900

    [FIX] 일반 고객 회원가입 페이지 Axios 추가

--- src/pages/customersignup/customersignup.js
+++ src/pages/customersignup/customersignup.js
@@ -6,6 +6,32 @@ import Statusbar from '../../components/statusbar/statusbar';
 import TopNav from '../../components/topnavigation/topnav';
 
 function Customersignup(){
+    const [customerName, setCustomerName] = useState('');
+    const [customerPassword, setCustomerPassword] = useState('');
+    const [customerIdEmail, setCustomerIdEmail] = useState('');
+    const [customerPhonenum, setCustomerPhonenum] = useState('');
+    const navigate = useNavigate();
+
+    const handleSignUp = async () => {
+        console.log("됨?");
+        try {
+            const response = await axios.post('http://3.36.175.224:8080/customer/signup', {
+                customerName,
+                customerIdEmail,
+                customerPassword,
+                customerPhonenum,
+            },{
+                headers: {
+                    'Content-Type': 'application/json'
+        },
+    });
+            console.log("됨2?");
+            console.log(response.data);
+            navigate('/home');
+        } catch (error) {
+            console.error('회원가입 오류:', error);
+        }
+    };
     
     return(
         <div >
@@ -22,14 +48,14 @@ function Customersignup(){
                 <input className={styles.inputBox} type='number' name = "pwd" size = '50'  placeholder='전화번호'></input>
                 <input className={styles.inputBox} type='email' name = "email" size = '50'  placeholder='Email'></input>
                 <input className={styles.inputBox} type='number' name = "validnum" size = '50'  placeholder='인증 번호 입력'></input>
-                <input className={styles.inputBox} type='password' name = "pwd" size = '50'  placeholder='Password'></input>
+                <input className={styles.inputBox} type='password' name = "pwd" size = '50'  placeholder='비밀번호'></input>
                 </form>
                 <div style={{width:'100%', height:'66px'}}></div>
             </div>
             <div style={{width:'100%', height:'10.0em'}}></div>
             <div style={{width:'100%', textAlign:'center'}}>
             <div className={styles.ButtonContainer}>
-                <div className={styles.Button}>일반 고객 회원 등록 하기</div>
+                <div className={styles.Button} onClick={handleSignUp}>일반 고객 회원 등록 하기</div>
             </div>
             {/* <input className = {styles.normalButton} type='button' value="일반 고객 회원가입" style={{margin: '8px'}}></input> */}
             </div> 
