545a20efbd3f477577a7ccbbfba2662942f9f150
choiyunz <yyy0304@ajou.ac.kr>
2023-11-23 14:24:40 +0900

    [Design]분석 결과 페이지 UI

--- src/pages/pslens/pslensresults.js
+++ src/pages/pslens/pslensresults.js
@@ -15,25 +15,37 @@ function PSLensResults (){
 
             <div>
                 <div className={styles.ImageWrap} style={{height: "20rem", borderRadius:"0rem"}}>
-                    분석끝난 이미지 결과
+                    <img src='/assets/test.png'></img>
                 </div>
 
-                <div style={{fontSize:"1rem", textAlign:"center", marginTop: "1.5rem"}}>--------- 위의 사진에 포함된 꽃입니다 --------- </div>
+                {/* <div style={{fontSize:"1rem", textAlign:"center", marginTop: "1rem", paddingTop:"1rem"}}>--------- 위의 사진에 포함된 꽃입니다 ---------- </div> */}
                 <div className={styles.ResultWrap}>
 
                     <div className={styles.ResultBox}>
-                        <div className={styles.FlowerBox}>
-                            <img scr='/assets/exampleimage.png'></img>
-                        </div>
-                        <div className={styles.ResultText}>꽃 결과</div>
+                        
+                        <img src='/assets/result1.jpeg' className={styles.FlowerBox}></img>
+                        <div className={styles.ResultText}>캐모마일 - 역경에 굴하지 않는 강인함</div>
                     </div>
 
                     <div className={styles.ResultBox}>
-                        <div className={styles.FlowerBox}>
-                            <img scr='/assets/exampleimage.png'></img>
-                        </div>
-                        <div className={styles.ResultText}>꽃 결과</div>
+                        
+                        <img src='/assets/result2.jpeg' className={styles.FlowerBox}></img>
+                        
+                        <div className={styles.ResultText}>작약 - 수줍음, 부끄러움</div>
                     </div>
+
+                    <div className={styles.ResultBox}>
+                        
+                        <img src='/assets/result3.jpeg'className={styles.FlowerBox}></img>
+                        <div className={styles.ResultText}>거베라 - 신비, 풀 수 없는 수수께끼</div>
+                    </div>
+
+                    <div className={styles.ResultBox}>
+                        <img src='/assets/result4.jpeg' className={styles.FlowerBox}></img>
+                        <div className={styles.ResultText}>하얀 장미 - 순수, 젊음, 새로운 시작</div>
+                    </div>
+
+
                 </div>
 
 
