fb864e1f355a5a265dabd73d99b62785a52c7f96
choiyunz <yyy0304@ajou.ac.kr>
2023-12-01 12:54:49 +0900

    [Fix]완제품 상세페이지 기능 수정

--- src/pages/detail/detail.js
+++ src/pages/detail/detail.js
@@ -7,6 +7,43 @@ import TopNav from '../../components/topnavigation/topnav';
 
 function ProductDetail(){
 
+    const navigate = useNavigate();
+    const [productDetails, setProductDetails]=useState({
+        fpName: '',
+        fpDescription: '',
+        fpFlowers: [],
+        fpTags: [],
+        fpPrice: 0,
+        pickupDate: '',
+        pickupTime: '',
+    });
+
+    useEffect(()=> {
+        const fetchData = async ()=> {
+            try {
+                const response = await axios.get('http://3.36.175.224:8080/endpoint주소/detail');
+                if (response.status==200){
+                    setProductDetails(response.data);
+                }
+
+            }catch (error) {
+                console.log('상품 상세 fetch error');
+            }
+        };
+
+    fetchData();},[]);
+
+    const addToCart = async()=>{
+        try {
+            await.axios.post('http://3.36.175.224:8080/endpoint주소/add-to-cart',{
+                productId : productDetails.fpKey,
+            });
+            navigate('/cart');
+        }catch(error){
+            console.log('장바구니에 완제품 담기 오류');
+        }
+    };
+
     return (
         <div>
             <Statusbar/>
@@ -18,20 +55,18 @@ function ProductDetail(){
             <div className={styles.Line}/>
 
             <div className={styles.DetailWrap}>
-                <div className={styles.FpName}>완제품상품명</div>
-                <div className={styles.FpDescription}>상품 상세 설명란</div>
+                <div className={styles.FpName}>{productDetails.fpName}</div>
+                <div className={styles.FpDescription}>{productDetails.fpDescription}</div>
                 <div className={styles.FpFlowerWrap}>
-                    <div className={styles.FpFlower}>레드로즈</div>
-                    <div className={styles.FpFlower}>핑크로즈</div>
-                    <div className={styles.FpFlower}>화이트로즈</div>
-                    <div className={styles.FpFlower}>꽃이름1</div>
-                    <div className={styles.FpFlower}>꽃이름2</div>
+                    {productDetails.fpFlowers.map((flower,index)=>(
+                        <div key = {index} className={styles.FpFlower}>{flower}</div>
+                    ))}
                 </div>
                 
                 <div className={styles.TagWrap}>
-                    <div className={styles.FpTag}>#발렌타인데이</div>
+                    <div className={styles.FpTag}>{productDetails.fpTag}</div>
                 </div>
-                <div className={styles.FpPrice}>35000₩</div>
+                <div className={styles.FpPrice}>{productDetails.fpPrice}원</div>
             </div>
 
 
@@ -58,7 +93,7 @@ function ProductDetail(){
 
 
             
-            <div className={styles.PurchaseButton}>
+            <div className={styles.PurchaseButton} onClick={addToCart}>
                 <img src='/assets/shoppingcart.svg'className={styles.Image}/>
                 <div>장바구니에 담기</div>
             </div>
