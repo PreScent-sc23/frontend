3d33980a753f8c9fcbc662e36d8bd4ede263040c
lord-potato <148340414+lord-potato@users.noreply.github.com>
2023-12-01 18:40:29 +0900

    [CHORE] Seller Home 구조 수정

--- src/pages/SellerMain/sellerhome.js
+++ src/pages/SellerMain/sellerhome.js
@@ -9,13 +9,32 @@ import SellerBottomTap from '../../components/bottomtap/sellerbottomtap';
 
 function SellerHome(){
     const navigate = useNavigate(); 
+    const [profilePhoto, setProfilePhoto] = useState('');
+    const [userName, setUserName] = useState('');
+
+    useEffect(() => {
+        axios.get('정보가 들어있는 주소', {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+        .then(response => {
+            // Replace 'profilePhoto' and 'userName' with the actual keys in your response data
+            setProfilePhoto(response.data.profilePhoto);
+            setUserName(response.data.userName);
+        })
+        .catch(error => {
+            console.error('Error fetching user data:', error);
+        });
+    }, []);
+
     return(
         <div>
             <Statusbar/>
             <TopNav/>
             <div className={styles.profileContainer}>
-                <div className={styles.profilePhoto}></div> 
-                <div style={{fontSize:'18px', width:'10rem', height:'1.8rem', backgroundColor:'#E2E2E2', borderRadius:'8px', lineHeight:'1.8rem'}}>김사업자</div>
+            <img src={profilePhoto} alt="Profile" className={styles.profilePhoto} /> 
+                <div style={{fontSize:'18px', width:'10rem', height:'1.8rem', backgroundColor:'#E2E2E2', borderRadius:'8px', lineHeight:'1.8rem'}}>{userName}</div>
             </div>
             <div style={{width:'100%', display:'flex', flexDirection: 'column', }}>
                 <div className={styles.selectBox} style={{backgroundImage: `url(/imgs/sellermenu_1.png)`}} onClick={()=>navigate('/shopregister')}>
@@ -24,7 +43,7 @@ function SellerHome(){
                 <div className={styles.selectBox} style={{backgroundImage: `url(/imgs/sellermenu_2.png)`}} onClick={()=>navigate('/manageproduct')}>
                     <div className={styles.textBox}>상품 관리</div>
                 </div>
-                <div className={styles.selectBox} style={{backgroundImage: `url(/imgs/sellermenu_3.png)`}} onClick={()=>navigate('/managereserve')}>
+                <div className={styles.selectBox} style={{backgroundImage: `url(/imgs/sellermenu_3.png)`}} onClick={()=>navigate('/sellerhome/history')}>
                     <div className={styles.textBox}>예약 관리</div>
                 </div>
                 <div className={styles.selectBox} style={{backgroundImage: `url(/imgs/sellermenu_4.png)`}} onClick={()=>navigate('/managestat')}>
