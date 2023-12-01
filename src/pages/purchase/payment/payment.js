c3c483016982fb52702b80a133949a6154650e55
choiyunz <yyy0304@ajou.ac.kr>
2023-12-01 13:13:43 +0900

    [Feat]카드 결제 기능 추가중

--- src/pages/purchase/payment/payment.js
+++ src/pages/purchase/payment/payment.js
@@ -7,7 +7,41 @@ import Searchbar from '../../../components/search/searchbar';
 import CustomerBottomTap from '../../../components/bottomtap/customerbottomtap';
 import TopNav from '../../../components/topnavigation/topnav';
 
+
+
+
 function Payment(){
+    function onClickPayment(){
+        //1.가맹점 식별
+        const {IMP}=window;
+        IMP.init(imp08177703);
+
+        //2. 결제 데이터 정의
+
+        const data = {
+            pg: 'html5_inicis',                           // PG사
+            pay_method: 'card',                           // 결제수단
+            merchant_uid: `mid_${new Date().getTime()}`,   // 주문번호
+            amount : '1000',                                 // 결제금액
+            name: '아임포트 결제 테스트',                  // 주문명
+            buyer_name: '홍길동',                           // 구매자 이름
+            buyer_tel: '01012341234',                     // 구매자 전화번호
+            buyer_email: 'example@example',               // 구매자 이메일
+            buyer_addr: '신사동 661-16',                    // 구매자 주소
+            buyer_postcode: '06018',                      // 구매자 우편번호
+          };
+          //4. 결제 창 호출
+          IMP.request_pay(data,callback);
+    }
+    //3, 콜백 함수 
+    function callback(response){
+        const {success, imp_uid, merchant_uid,pay_method, paid_amount,status,error_msg}=response;
+        if (success){
+            alert('결제 성공');
+        }else{
+            alert(`결제 실패 : ${error_msg}`);
+        }
+    }
 
 
     return (
@@ -53,7 +87,7 @@ function Payment(){
                 </div>
                 <div className={styles.Line}></div>
 
-                <div className={styles.PolicyTitle}>주문 내용을 확인하였으며, 아래 내용에 동의합니다.</div>
+                {/* <div className={styles.PolicyTitle}>주문 내용을 확인하였으며, 아래 내용에 동의합니다.</div>
                 <div className={styles.Wrap}>
                     <div className={styles.PolicyWrap}>
                         <img src='/assets/circle.svg'></img>
@@ -70,13 +104,13 @@ function Payment(){
                         <img src='/assets/circle.svg'></img>
                         <div className={styles.PolicyText}>(필수) 개인정보 제공 및 위탁안내</div>
                     </div>
-                </div>
+                </div> */}
 
                 <div className={styles.Line}></div>
 
-                <div className={styles.Button}>
+                <div className={styles.Button} onClick={onClickPayment}>
                     <img src='/assets/credit-card.svg'></img>
-                    <span>카드 결제</span>
+                    <span>결제 하기</span>
                 </div>
                 
 
