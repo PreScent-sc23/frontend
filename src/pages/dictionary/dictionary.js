6e215b72addf4b0bfa581e4d333b6bbd88dd90dd
lord-potato <148340414+lord-potato@users.noreply.github.com>
2023-12-03 13:05:17 +0900

    [FEAT]꽃말 사전 텍스트 추가 및 팝업 구현

--- src/pages/dictionary/dictionary.js
+++ src/pages/dictionary/dictionary.js
@@ -10,7 +10,17 @@ import CustomerBottomTap from '../../components/bottomtap/customerbottomtap';
 function Dictionary(){
     
     const navigate = useNavigate(); 
-    const pageinfo = 'PS사전에서 궁금한 꽃에 대한 정보를 찾아서\n꽃에 담긴 진정한 의미를 알아보세요!\n꽃의 정보는 정기적으로 업데이트 됩니다.'
+    const [selectedFlower, setSelectedFlower] = useState(null);
+    const pageinfo = 'PS사전에서 궁금한 꽃에 대한 정보를 찾아\n꽃에 담긴 진정한 의미를 알아보세요!\n꽃의 정보는 정기적으로 업데이트 됩니다.'
+    
+    const handleFlowerClick = (flower) => {
+        setSelectedFlower(flower);
+    }
+ 
+    const handleClosePopup = () => {
+        setSelectedFlower(null);
+    }
+    
     return (
         <div>
             <Statusbar/>
@@ -20,7 +30,7 @@ function Dictionary(){
             </div>
             <div className={styles.ListContainer}>
                 {flowers.map((flower, index) => (
-                    <div key = {index}>
+                    <div key = {index} onClick={() => handleFlowerClick(flower)}>
                         <div className={styles.FlowerContainer}>
                             <div className={styles.ImageContainer}>
                                 <img src={flower.imgpath} alt={flower.name} />
@@ -35,8 +45,32 @@ function Dictionary(){
                             </div>
                         </div>
                     </div>
-                ))}
+                ))} 
             </div>
+            
+            {selectedFlower && (
+               <div style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                  <div style={{ display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', backgroundColor: 'white', width:'370px', height:'700px',padding: '20px', borderRadius: '12px' }}>
+                    <div className={styles.PopImageContainer}>
+                        <img src={selectedFlower.imgpath} alt={selectedFlower.name} />
+                    </div>
+                    <div className={styles.LogoContainer} style={{width:'410px', marginTop:'12px'}}>
+                        <img src='/imgs/logo.png' style={{height:'60px'}}></img>
+                    </div>
+                    <div className={styles.PopDetail}>  
+                        <div style={{fontSize:'25px', fontWeight:'bold', borderBottom:'2px solid #FF9494'}}>
+                            {selectedFlower.name}
+                        </div>
+                        <div style={{height:'30px'}}>
+                        </div>
+                        <div style={{height:'180px', width:'80%' ,border:'2px solid #FED8DF', borderRadius:'12px',padding:'8px', display:'flex', justifyContent:'center', alignItems:'center', fontSize:'14px'}}>
+                            {selectedFlower.description}
+                        </div>
+                    </div>
+                    <button className={styles.PopClose} onClick={handleClosePopup}>뒤로 가기</button>
+                  </div>
+               </div>
+           )}
 
             <div className={styles.PageInfo}>
                 {pageinfo}
--- src/pages/dictionary/flowers.js
+++ src/pages/dictionary/flowers.js
@@ -1,7 +1,7 @@
 export const flowers = [
-    { name: '장미', description: '장미 입니다.',tag: '사랑', imgpath: '/dictionary/rose.jpeg' },
-    { name: '개나리', description: '개나리 입니다.', tag: '희망', imgpath: '/dictionary/forsydia.jpeg'},
-    { name: '카네이션', description: '카네이션 입니다.', tag: '존경', imgpath: '/dictionary/carnation.jpeg'},
+    { name: '장미', description: '사랑을 표현할 때 자주 사용되는 꽃입니다.\n상응하는 마음이 있으시다면\n돌려주는 것도 나쁘지 않겠죠?\n\n꽃말로는\n"사랑, 아름다움, 용기, 존경" 이 있어요!',tag: '사랑', imgpath: '/dictionary/rose.jpeg' },
+    { name: '개나리', description: '한 해를 기운차게 시작하라는 의미로\n주로 사용되는 꽃입니다.\n길거리를 아름답게 장식하는 꽃이기도 하죠.\n\n꽃말로는\n"기대, 희망, 깊은 정" 이 있어요!', tag: '희망', imgpath: '/dictionary/forsydia.jpeg'},
+    { name: '카네이션', description: '사랑을 쏟은 이에게 이 꽃을 받았다면\n먼저 축하를 드리고 싶어요.\n평소에 감사를 느낀 사람에게 많이 선물하는 \n꽃이기 때문이죠.\n\n꽃말로는\n"모정, 사랑, 감사, 존경" 이 있어요!', tag: '존경', imgpath: '/dictionary/carnation.jpeg'},
     { name: '안개꽃', description: '안개꽃 입니다.', tag: '마음', imgpath: '/dictionary/babys_breath.jpeg'},
     { name: '튤립', description: '튤립 입니다.', tag: '사랑', imgpath: '/dictionary/tulip.jpeg'},
     { name: '백합', description: '백합 입니다.', tag: '순결', imgpath: '/dictionary/lily.jpeg'},
